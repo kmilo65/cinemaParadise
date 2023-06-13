c6d995e3b7e154f6ef65a15dddfa4e23b16478de
kmilo <cfhg65@gmail.com>
2023-06-13 09:42:03 -0400

    updated footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ export const Footer = () => {
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+          <Link to="/" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</Link>
         </li>
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+          <Link to="/" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</Link>
         </li>
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Youtube</a>
+          <Link to="/" target="_blank" className="mr-4 hover:underline md:mr-6">Youtube</Link>
         </li>
         <li>
-          <a href="#" target="_blank" className="hover:underline">Github</a>
+          <Link to="/" target="_blank" className="hover:underline">Github</Link>
         </li>
       </ul>
     </footer>
